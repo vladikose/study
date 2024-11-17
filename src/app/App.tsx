64f5741f6226a46fa985a9882fd9514413fb3e71
vladikose <vladiiik27@gmail.com>
2024-11-17 17:56:39 +0300

    feature sliced

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,17 @@
-import React, {Suspense, useContext, useState} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/routers";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
+            <AppRouter/>
             <button onClick={toggleTheme}>toggle</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О компании</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/'} element={<MainPage />}/>
-                    <Route path={'/about'} element={<AboutPage />}/>
-                </Routes>
-            </Suspense>
         </div>
     );
 };
