f03922900679a302b63ae00c66a617cb15e71259
vladikose <vladiiik27@gmail.com>
2024-11-27 13:49:07 +0300

    small fix

--- src/app/providers/routers/ui/AppRouter.tsx
+++ src/app/providers/routers/ui/AppRouter.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = () => (
-    <Suspense fallback="">
+    <Suspense fallback="Loading...">
         <Routes>
             {Object.values(routeConfig).map(({ element, path }) => (
                 <Route
