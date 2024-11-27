1b379377123a7c528df7a0028dea6746b8cce70d
vladikose <vladiiik27@gmail.com>
2024-11-27 13:38:10 +0300

    small fix and migrate to work pc

--- src/app/providers/routers/ui/AppRouter.tsx
+++ src/app/providers/routers/ui/AppRouter.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback="">
         <Routes>
             {Object.values(routeConfig).map(({ element, path }) => (
                 <Route
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { useState } from 'react';
 import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -13,12 +14,13 @@ export const Sidebar = ({ className }: SidebarProps) => {
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
+    const { t } = useTranslation();
 
     return (
         <div
             className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
-            <button type="button" onClick={onToggle}>toggle</button>
+            <button type="button" onClick={onToggle}>{t('перекл')}</button>
 
             <div className={cls.switchers}>
                 <ThemeSwitcher className={cls.theme_switcher} />
