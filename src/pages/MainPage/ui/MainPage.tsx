f261ec0a5e48ed89c58b1c45716e2b798aa577dc
vladikose <vladiiik27@gmail.com>
2024-12-11 14:45:52 +0300

    better theme styles

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { BugButton } from 'app/providers/ErrorBoundary';
+import Products from 'shared/lib/data/data';
 
 const MainPage = () => {
     const { t } = useTranslation();
@@ -9,6 +10,7 @@ const MainPage = () => {
         <div>
             <h2>{t('Главная')}</h2>
             <BugButton />
+            <Products />
         </div>
     );
 };
