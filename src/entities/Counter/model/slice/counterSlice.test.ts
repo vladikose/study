f79b7e03656679ae0ec0cd10bb53cb74c065397e
vladikose <vladiiik27@gmail.com>
2025-05-13 16:06:53 +0300

    fix pipeline 3

--- src/entities/Counter/model/slice/counterSlice.test.ts
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -1,5 +1,5 @@
+import { counterReducer, counterActions } from './counterSlice';
 import { CounterSchema } from '../types/counterSchema';
-import { counterReducer, counterActions } from './CounterSlice';
 
 describe('counterSlice.test', () => {
     test('decrement', () => {
