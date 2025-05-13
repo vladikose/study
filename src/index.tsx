df19a21df5db3ccd70642b7207bb1abe1e30b5f6
vladikose <vladiiik27@gmail.com>
2025-05-13 15:11:22 +0300

    add redux store, add entity (counter)

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,17 @@ import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import App from './app/App';
 import 'shared/config/i18n/i18n';
 import './app/styles/index.scss';
+import { StoreProvider } from 'app/providers/StoreProvider';
 
 render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
     document.getElementById('root'),
 );
