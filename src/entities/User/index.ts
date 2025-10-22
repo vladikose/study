a0a8addabf4cb064583b1d6a2aae1f1c69e95eca
vladikose <vladiiik27@gmail.com>
2025-10-22 12:56:39 +0300

    update app router

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -3,9 +3,14 @@ export {
 } from './model/selectors/getUserAuthData/getUserAuthData';
 
 export {
+    getUserInited,
+} from './model/selectors/getUserInited/getUserInited';
+
+export {
     userReducer,
     userActions,
 } from './model/slice/userSlice';
+
 export {
     User,
     UserSchema,
